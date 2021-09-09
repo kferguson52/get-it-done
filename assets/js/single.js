d78ec673bb2be80862594bf19c8def255d0f4128
Kyle <kferguson@captechconsulting.com>
2021-09-09 16:49:00 -0500

    finished section 6.3 with DOM and additonal js file

---
+++ assets/js/single.js
@@ -0,0 +1,61 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+var getRepoIssues = function (repo) {
+  console.log(repo);
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayIssues(data);
+      });
+    } else if (displayWarning(repo)) {
+      console.log("repo has more than 30 issues");
+    } else {
+      alert("There was a problem with your request!");
+    }
+  });
+};
+
+getRepoIssues("facebook/react");
+
+var displayIssues = function (issues) {
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or a pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+    issueContainerEl.appendChild(issueEl);
+  }
+};
